688b28c5cb25ad8afe0877924c0b8637faeeb83c
N.M. Porterfield <ccmstudiosinc@gmail.com>
2021-03-19 01:33:19 -0400

    twitch player

--- gaming/src/pages/games.js
+++ gaming/src/pages/games.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from '../components/navbar'
-
+import ReactPlayer from "react-player"
 
 class Games extends React.Component{
     render(){
         return(
             <div>
-                <h1>Twitch Goes here</h1>
+                <h1>Watch Me Live</h1>
+                <h3>Fridays,Saturdays, and Sundays at 10pm EST</h3>
+                <ReactPlayer
+        url="https://www.twitch.tv/urfavoriteauthor"
+        controls
+      />
                 <div>
 <h1>Carousel of highlights goes here</h1>
                 </div>
